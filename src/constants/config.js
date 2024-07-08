370c54a4541b0588242be44161ed903a94709a72
Arihant-2004 <arihanthirawat4@gmail.com>
2024-07-08 16:43:02 +0530

    local

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,3 @@
 //  export const server ="https://chatapp-4-ui1x.onrender.com"
- export const server ="https://chatapp-5-yfwt.onrender.com"
+//  export const server ="https://chatapp-5-yfwt.onrender.com"
+export const server ="http://localhost:3000"
